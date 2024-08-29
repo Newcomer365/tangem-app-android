8963cbf680e02db5ce0971a679405f22d26dd0be
Mama1emon <andrew.khokhlove@gmail.com>
2024-08-29 15:03:21 +0400

    test
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

---
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/PortfolioBSVisibilityModel.kt
@@ -0,0 +1,9 @@
+package com.tangem.features.markets.portfolio.impl.model
+
+/**
+ * @author Andrew Khokhlov on 28/08/2024
+ */
+data class PortfolioBSVisibilityModel(
+    val addToPortfolioBSVisibility: Boolean = false,
+    val walletSelectorBSVisibility: Boolean = false,
+)
